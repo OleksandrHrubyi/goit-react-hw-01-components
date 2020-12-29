4cc0023985cd2da7d3441ae4bfc186460a4e6c01
OleksandrGrubyi <ghrubyi@ukr.net>
2020-12-29 17:27:22 +0200

    change app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,11 +9,11 @@ import transactionList from 'data/transaction.json';
 
 export default function App() {
   return (
-    <div>
+    <>
       <Profile user={user} />
       <Statistics title="Upload stats" stats={statData} />
       <FriendList list={friendData} />
       <Transaction list={transactionList} />
-    </div>
+    </>
   );
 }
